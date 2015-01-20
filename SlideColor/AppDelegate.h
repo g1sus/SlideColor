c1890a1b1852ff20869e6fdac846e05dc923bc35
g1sus <proyectos_technology@hotmail.com>
2015-01-19 21:23:11 -0600

    Initial Commit

---
+++ SlideColor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlideColor
+//
+//  Created by Jesus Antonio Hernandez Mendez on 19/01/15.
+//  Copyright (c) 2015 BTICs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
